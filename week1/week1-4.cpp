a65ac975b9bac00fb37e7b040abe3309445ef402
25A31A05JU <anushasapireddy4123@gmail.com>
2026-03-13 19:42:09 +0530

    Add files via upload

---
+++ week1/week1-4.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+	iny a,b,c;
+	cout<<"enter a,b,c values";
+	cin>>a>>b>>c;
+	if((a>b)&&(a>c)){
+		cout<<"a is big";
+	}
+		else if(b>c){
+			cout<<"b is big";
+		
+	}
+	else {
+		cout<<"c is big";
+	}
+}
